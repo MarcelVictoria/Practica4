64282bb3bd6382640dc44415af88b401ee98d6ce
MarcelVictoria <126016555+MarcelVictoria@users.noreply.github.com>
2023-06-07 22:55:26 +0200

    Add files via upload

---
+++ Practica4/src/html.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+// HTML & CSS contents which display on web server
+String HTML = "<!DOCTYPE html>\
+<html>\
+<body>\
+<h2>The src Attribute</h2>\
+<p>HTML images are defined with the img tag, and the filename of the image source is specified in the src attribute:</p>\
+<img src=https://i.pinimg.com/564x/d9/d8/9a/d9d89a3d138468ad2b4f668821f589a7.jpg width=500 height=600>\
+</body>\
+</html>";
+
